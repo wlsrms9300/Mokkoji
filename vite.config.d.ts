d227e755f65791182cc4d2a5eb718aa9d23178fa
Jingeun <wlsrms9300@naver.com>
2025-05-08 16:05:59 +0900

    build 명령어 수정, workflows 빌드 테스트

---
+++ vite.config.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @see {@link https://vitejs.dev/config/}
+ * @see {@link https://ko.vitejs.dev/config/build-options.html#build-rollupoptions} // vite rollup 옵션설정
+ * @see {@link https://rollupjs.org/configuration-options/} // rollup 옵션설정
+ */
+declare const _default: import("vite").UserConfigFnPromise;
+export default _default;
